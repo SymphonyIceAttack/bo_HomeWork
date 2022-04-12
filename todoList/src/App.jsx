8872e3d694d9638a030d0341a557e5a1eda53205
幻彩冰袭 <1994370030@qq.com>
2022-04-12 23:28:56 +0800

    2

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import logo from "./logo.svg";
+import Container from "./container";
+
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <div className="App">
-      
-    </div>;
+    return (
+        <div className="App">
+            <Container />
+        </div>
+    );
 }
 
 export default App;
---
+++ todoList/src/container/AddItem/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.less"
+
+
+const index = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default index
